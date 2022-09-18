f6f8c4134a0079f837f7cda9bc4b698e6375d03e
Richard Artoul <richardartoul@gmail.com>
2022-09-18 14:07:04 -0500

    fix

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ function onChange() {
         "numCores": document.getElementById("numCores").value,
         "numCoresPerServer": document.getElementById("numCoresPerServer").value,
         "numServersPerRack": document.getElementById("numServersPerRack").value,
-        "renderLights": document.getElementById("renderLights").value === "on" ? true : false,
-        "renderServers": document.getElementById("renderServers").value === "on" ? true : false,
     })
-    console.log("newURL", newURL);
-    console.log(document.getElementById("renderServers").value);
     document.getElementById("visualization").src = newURL;
 
     const shareURL = "https://richardartoul.github.io/core-count/" + newURL;;
